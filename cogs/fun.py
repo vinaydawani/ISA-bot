a1c8f62783051bd4bb1f632773adce07692cb2db
vinaydawani <vdawani6@gmail.com>
2020-05-23 23:31:51 +0530

    cat and dog commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -63,6 +63,66 @@ class fun(commands.Cog):
             else:
               await send_embedded(ctx, 'That is NOT one of the dank textfaces in here yet. Use !!textface list to see a list of the textfaces.')
 
+    @commands.group(case_insensitive=True, invoke_without_command=True)
+    @commands.cooldown(3, 10, commands.BucketType.user)
+    async def cat(self, ctx):
+        await ctx.send_help(ctx.command)
+
+    async def get_cat(self, ctx, r):
+        if r.status_code == 200:
+            j = r.json()
+        else:
+            return await send_embedded(ctx, f"Error {r.status_code} in fetching the image. Try again later please.")
+
+        embed = discord.Embed(title="A wild cat has appeared :cat:",
+                                color=random.choice(self.bot.color_list))
+        embed.set_image(url=j[0]['url'])
+        embed.set_footer(text=f"Image requested by {ctx.author}\nFetched from TheCatApi")
+        await ctx.send(embed=embed)
+        await ctx.message.delete(delay=60.0)
+
+    @cat.command(name='gif')
+    async def _gif(self, ctx):
+        header = {"x-api-key": self.bot.config['keys']['cat_api']}
+        r = requests.get(url='https://api.thecatapi.com/v1/images/search?mime_types=gif', headers=header)
+        await self.get_cat(ctx, r)
+
+    @cat.command(name='pic')
+    async def _pic(self, ctx):
+        header = {"x-api-key": self.bot.config['keys']['cat_api']}
+        r = requests.get(url='https://api.thecatapi.com/v1/images/search?mime_types=jpg,png', headers=header)
+        await self.get_cat(ctx, r)
+
+    @commands.group(case_insensitive=True, invoke_without_command=True)
+    @commands.cooldown(3, 10, commands.BucketType.user)
+    async def dog(self, ctx):
+        await ctx.send_help(ctx.command)
+
+    # IDEA: Allow people to list breeds and search by breeds
+    async def get_dog(self, ctx, r):
+        if r.status_code == 200:
+            j = r.json()
+        else:
+            return await send_embedded(ctx, f"Error {r.status_code} in fetching the image. Try again later please.")
+
+        embed = discord.Embed(title="OMG a doggo :dog:",
+                                color=random.choice(self.bot.color_list))
+        embed.set_image(url=j[0]['url'])
+        embed.set_footer(text=f"Image requested by {ctx.author}\nFetched from TheCatApi")
+        await ctx.send(embed=embed)
+        await ctx.message.delete(delay=60.0)
+
+    @cat.command(name='gif')
+    async def _gif(self, ctx):
+        header = {"x-api-key": self.bot.config['keys']['dog_api']}
+        r = requests.get(url='https://api.thedogapi.com/images/search?mime_types=gif', headers=header)
+        await self.get_dog(ctx, r)
+
+    @cat.command(name='pic')
+    async def _pic(self, ctx):
+        header = {"x-api-key": self.bot.config['keys']['dog_api']}
+        r = requests.get(url='https://api.thedogapi.com/images/search?mime_types=jpg,png', headers=header)
+        await self.get_dog(ctx, r)
 
 
 def setup(bot):
